0d652168419ed99b8c55a5af2aa1c7722fcd4441
Ayazzaman <saeedsid215@gmail.com>
2025-01-29 21:40:35 -0330

    block and position

---
+++ Tetris/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+
+            }
+        }
+    }
+}
